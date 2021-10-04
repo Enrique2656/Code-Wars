cdece9f7a3eafbb0346ba241d21c7b820dcd8a7b
enrique2656 <duaneenrique26@gmail.com>
2021-10-04 15:59:33 -0400

    codewars

--- longmultiplication.js
+++ longmultiplication.js
@@ -1,5 +1,5 @@
 // This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
-
+// my solution:
 
 function simpleMultiplication(number) {
     if (number % 2 === 0){
