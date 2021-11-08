0ef80c58a873ea8a4080cca5b515e947bc85a758
enrique2656 <duaneenrique26@gmail.com>
2021-11-08 14:59:54 -0500

    remove string spcaes

---
+++ RemoveStringSpaces.js
@@ -0,0 +1,10 @@
+Description: 
+// Simple, remove the spaces from the string, then return the resultant string.
+
+function noSpace(x){
+    return x.split(' ').join('');
+}
+  // removes spaces from string then return string without space.
+  // any commas? - no
+  // [a b c]
+  // [abc]
