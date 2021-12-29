604513bab42301ac3b4851e68411180f6dc952b1
enrique2656 <duaneenrique26@gmail.com>
2021-12-28 22:51:10 -0500

    Write a function findNeedle() that takes an array full of junk but containing one needle

--- lostWOaMap.js
+++ lostWOaMap.js
@@ -1,8 +1,8 @@
-Given an array of integers, return a new array with each value doubled.
+// Given an array of integers, return a new array with each value doubled.
 
-For example:
+// For example:
 
-[1, 2, 3] --> [2, 4, 6]
+// [1, 2, 3] --> [2, 4, 6]
 
 
 function maps(x){
